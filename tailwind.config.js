797975e7066b9f81f8b9416b62d47b86f2a750ff
Althafav <61247025+althafav@users.noreply.github.com>
2022-12-08 18:28:13 +0530

    header done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: [],
   theme: {
     extend: {
+      fontFamily: {
+        'Roboto': ['Roboto', 'Arial', 'sans-serif']
+      },
       spacing : {
         '14': '3.5rem'
       }
