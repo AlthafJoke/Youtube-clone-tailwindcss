b7cfcaa37cf7c0b225a7813628aff5e336d027e3
Althafav <61247025+althafav@users.noreply.github.com>
2022-12-08 20:00:28 +0530

    sidebar is done but some changes needed

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,9 @@ module.exports = {
         'Roboto': ['Roboto', 'Arial', 'sans-serif']
       },
       spacing : {
-        '14': '3.5rem'
+        '14': '3.5rem',
+        '18': '4.5rem',
+
       }
     },
   },
